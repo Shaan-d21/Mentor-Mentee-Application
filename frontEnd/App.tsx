c224ae4cc7d0be1813f336ef7b173d9b7f1d3686
kavan2003 <kavanthosani2003@gmail.com>
2025-03-22 13:27:34 +0530

    Restructured Folders and removed not needed files

--- frontEnd/App.tsx
+++ frontEnd/App.tsx
@@ -1,14 +1,11 @@
 import React from 'react';
 import { SafeAreaView, StatusBar } from 'react-native';
-import AuthScreen from './src/screens/AuthScreen';
 
-//
 
 const App: React.FC = () => {
   return (
     <SafeAreaView style={{ flex: 1 }}>
-      <StatusBar barStyle="dark-content" />
-      <AuthScreen />
+    
     </SafeAreaView>
   );
   //
