6a9e47e6a6f83c7cf4c62e0250e29fc00bd495a0
sayantan karmakar <skarmakar1107@gmail.com>
2025-03-27 23:36:40 +0530

    fix: modified database and main file

--- backend/FastAPI for login and mentor profile/main.py
+++ backend/FastAPI for login and mentor profile/main.py
@@ -1,12 +1,11 @@
 from fastapi import FastAPI
 import models
 from database import engine
-from Routers import auth, user
-
+from Routers import auth, get_approved_mentees, user
 
 app = FastAPI()
 app.include_router(auth.router)
 app.include_router(user.router)
-
+app.include_router(get_approved_mentees.router)
 
 models.base.metadata.create_all(bind = engine)
