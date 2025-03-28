19cd86fdbf5b105cfa3ae745f8fc18cb10a97fa2
SrajanMishra <srajanmishra2003@gmail.com>
2025-03-28 20:38:01 +0530

    Endpoints For AI Model & Send Request to mentor

--- backend/FastAPI for login and mentor profile/main.py
+++ backend/FastAPI for login and mentor profile/main.py
@@ -1,12 +1,12 @@
 from fastapi import FastAPI
 import models
 from database import engine
-from Routers import auth, user
+from Routers import auth, user, mentee
 
 
 app = FastAPI()
 app.include_router(auth.router)
 app.include_router(user.router)
-
+app.include_router(mentee.router)
 
 models.base.metadata.create_all(bind = engine)
