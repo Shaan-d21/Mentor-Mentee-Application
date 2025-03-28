153aa85404111a32bf907ad9e3b7a244d6520028
sirtiox <sirtcseb@gmail.com>
2025-03-28 17:32:34 +0530

    feat: login and signup page updated

--- web/src/pages/dashboard/DashboardPage.tsx
+++
@@ -1,9 +0,0 @@
-export default () => {
-  return (
-    <>
-      <div className="flex flex-col items-center justify-center h-screen p-1 gap-2 text-3xl font-serif">
-        This is the dashboard page
-      </div>
-    </>
-  );
-};
