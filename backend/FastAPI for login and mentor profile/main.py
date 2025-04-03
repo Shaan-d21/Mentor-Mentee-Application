d670d45c859bb64e9794e4517eee7b8f8f9f80e0
Tom <tom@gmail.com>
2025-04-03 15:01:34 +0530

    Deploying to server

--- backend/FastAPI for login and mentor profile/main.py
+++ backend/FastAPI for login and mentor profile/main.py
@@ -1,10 +1,20 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 import models
 from database import engine
 from Routers import auth, user, mentee, mentor_approval,get_approved_mentees,get_requests, api
 
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:8081", "http://frontend:80", "http://181.214.44.15:8081"],  # Frontend URLs
+    allow_credentials=True,
+    allow_methods=["*"],  # Allow all methods
+    allow_headers=["*"],  # Allow all headers
+)
+
 app.include_router(auth.router)
 app.include_router(user.router)
 app.include_router(mentee.router)
