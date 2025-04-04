fbba9b7122b513778bf35a4d025fc8ac9289d97c
Shasank505 <shasankpandey777@gmail.com>
2025-04-04 16:54:45 +0530

    update config

--- backend/FastAPI for login and mentor profile/config.py
+++ backend/FastAPI for login and mentor profile/config.py
@@ -13,11 +13,11 @@ GOOGLE_API_KEY = os.getenv("GOOGLE_API_KEY")
 GEMINI_MODEL_NAME = 'models/gemini-1.5-flash-8b-exp-0827'
 
 # Database Configuration for PostgreSQL
-DATABASE_USER = os.getenv("DB_USER", "postgres")
-DATABASE_PASSWORD = os.getenv("DB_PASSWORD", "1234")
-DATABASE_HOST = os.getenv("DB_HOST", "localhost")
-DATABASE_PORT = os.getenv("DB_PORT", "5432")
-DATABASE_NAME = os.getenv("DB_NAME", "ProjectTest1")
+DATABASE_USER = os.getenv("DB_USER")
+DATABASE_PASSWORD = os.getenv("DB_PASSWORD")
+DATABASE_HOST = os.getenv("DB_HOST")
+DATABASE_PORT = os.getenv("DB_PORT")
+DATABASE_NAME = os.getenv("DB_NAME")
 
 # Construct the PostgreSQL connection string
 DATABASE_URL = f"postgresql://{DATABASE_USER}:{DATABASE_PASSWORD}@{DATABASE_HOST}:{DATABASE_PORT}/{DATABASE_NAME}"
