de49bf11a313a2d80ac7c881866b4523b7ed929a
Pratyusha Mondal <pratyusha@example.com>
2025-04-05 16:25:59 +0530

    Made new API endpoints

--- backend/FastAPI for login and mentor profile/main.py
+++ backend/FastAPI for login and mentor profile/main.py
@@ -1,9 +1,7 @@
 from fastapi import FastAPI
 import models
 from database import engine
-from Routers import auth, user, mentee, mentor_approval,get_approved_mentees,get_requests, api
-from Routers.roadmap_route import router as roadmap_router
-from config import GOOGLE_API_KEY, GEMINI_MODEL_NAME
+from Routers import auth, user, mentee, mentor_approval,get_approved_mentees, get_approved_mentors, get_requests, api, mentee_roadmap, assign_roadmap, roadmap_route
 
 app = FastAPI()
 app.include_router(auth.router)
@@ -11,8 +9,15 @@ app.include_router(user.router)
 app.include_router(mentee.router)
 app.include_router(mentor_approval.router)
 app.include_router(get_approved_mentees.router)
+app.include_router(get_approved_mentors.router)
 app.include_router(get_requests.router)
 app.include_router(api.router)
-app.include_router(roadmap_router)
+app.include_router(roadmap_route.router)
+app.include_router(mentee_roadmap.router)
+app.include_router(assign_roadmap.router)
+
+@app.get("/")
+def read_root():
+    return {"message": "Hello from FastAPI"}
 
 models.Base.metadata.create_all(bind = engine)
