fcf58a5d425db24aaf5a7adff5d5a76785df18e2
shaan <shaan.dewang212@gmail.com>
2025-04-07 15:11:19 +0530

    made changes in deployment file

--- backend/FastAPI for login and mentor profile/Routers/api.py
+++ backend/FastAPI for login and mentor profile/Routers/api.py
@@ -6,7 +6,6 @@ from starlette import status
 import pickle
 
 from Routers.auth import get_current_user
-from .mentor_mentee import matching
 
 router = APIRouter(
     prefix='/matching',
@@ -29,15 +28,3 @@ class Domains(str, Enum):
 class Domain(BaseModel):
     choise : Domains
 user_dependency = Annotated[dict, Depends(get_current_user)]
-
-@router.post('/predict')
-async def suggestion(user: user_dependency,d : Domain):
-    #try : 
-        if user is None or user.get('role') != 'mentee':
-            return HTTPException(status_code=401, detail="Authentication Error")
-        st=d.choise.value
-        mentor_list=matching(str(st)) #Making Predictions
-         #rounding of the probability
-        return mentor_list
-    # except: 
-    #     raise HTTPException( status_code=status.HTTP_400_BAD_REQUEST, detail="Invalid input")
