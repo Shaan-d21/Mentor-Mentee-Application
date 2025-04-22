395ffbfdd4d9b1819f4449fb9d376a2d25fc5697
kavan2003 <kavanthosani2003@gmail.com>
2025-04-22 17:15:00 +0530

    Uncommented store.ts line to resolve error in ViewRoadmap. Added Total Duration in ViewRoadmap

--- mobile/src/redux/store.ts
+++ mobile/src/redux/store.ts
@@ -34,4 +34,4 @@ export const store = configureStore({
 
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
-// export const useAppDispatch = () => useDispatch<AppDispatch>();
+export const useAppDispatch = () => useDispatch<AppDispatch>();
