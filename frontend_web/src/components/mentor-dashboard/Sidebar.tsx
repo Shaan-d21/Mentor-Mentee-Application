172eff0f6e3b4ef5f31f542e48e3e79dd9de0bce
Pratyusha Mondal <pratyusha@example.com>
2025-04-26 12:58:58 +0530

    Changed find mentor icon

--- frontend_web/src/components/mentor-dashboard/Sidebar.tsx
+++ frontend_web/src/components/mentor-dashboard/Sidebar.tsx
@@ -5,6 +5,7 @@ import {
   Users, 
   UserCheck, 
   ChevronLeft,
+  UserSearch,
   Menu,
   Map,
   FileText,
@@ -29,7 +30,7 @@ const mentorNavItems: NavItem[] = [
 
 const menteeNavItems: NavItem[] = [
   { name: 'Dashboard', path: '/mentee/dashboard', icon: <LayoutDashboard size={20} /> },
-  { name: 'Find Mentors', path: '/mentee/dashboard/find-mentors', icon: <Users size={20} /> },
+  { name: 'Find Mentors', path: '/mentee/dashboard/find-mentors', icon: <UserSearch size={20} /> },
   { name: 'My Requests', path: '/mentee/dashboard/requests', icon: <UserCheck size={20} /> },
   { name: 'My Mentors', path: '/mentee/dashboard/my-mentors', icon: <Users size={20} /> },
   { name: 'My Roadmaps', path: '/mentee/dashboard/roadmaps', icon: <Map size={20} /> },
